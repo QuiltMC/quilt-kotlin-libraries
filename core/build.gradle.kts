c8f00b2df8b6f5abb62177b9c2962cc077ebf58c
oliver <kookooloopdaking@gmail.com>
2022-06-09 20:03:44 -0500

    Coerce Gradle into listening to me

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,10 +1,21 @@
-import org.gradle.kotlin.dsl.DependencyHandlerScope
+plugins {
+    id("org.quiltmc.loom")
+}
 
 fun DependencyHandlerScope.includeApi(dependency: Any) {
     include(dependency)?.let { modApi(it) }
 }
 
 dependencies {
+    minecraft(rootProject.libs.minecraft)
+    mappings(loom.layered {
+        addLayer(quiltMappings.mappings("org.quiltmc:quilt-mappings:${rootProject.libs.versions.quilt.mappings.get()}:v2"))
+    })
+
+    modImplementation(rootProject.libs.quilt.loader)
+
+    modImplementation(rootProject.libs.qsl)
+
     val kotlinVersion = rootProject.libs.versions.kotlin.get()
     includeApi(kotlin("stdlib", kotlinVersion))
     includeApi(kotlin("stdlib-jdk8", kotlinVersion))
@@ -16,4 +27,8 @@ dependencies {
     includeApi(rootProject.libs.serialization.core.get())
     includeApi(rootProject.libs.serialization.json.get())
     includeApi(rootProject.libs.serialization.cbor.get())
+}
+
+tasks.remapJar {
+    archiveBaseName.set("quilt-kotlin-libraries-${name}")
 }
