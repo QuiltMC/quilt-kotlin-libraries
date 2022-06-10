76183f3ce3bb9cdcd18f1a3a9e7e2ac07a0a35c3
oliver <kookooloopdaking@gmail.com>
2022-06-10 14:26:36 -0500

    Publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,12 @@ plugins {
     alias(libs.plugins.quilt.loom)
     alias(libs.plugins.detekt)
     alias(libs.plugins.licenser)
+    id("maven-publish")
 }
 
 group = "org.quiltmc"
 version = project.version
+val projectVersion = project.version as String
 
 repositories {
     mavenCentral()
@@ -46,6 +48,60 @@ allprojects {
         inputs.property("version", version)
         filesMatching("quilt.mod.json") {
             expand(Pair("version", version))
+        }
+    }
+}
+subprojects {
+    apply(plugin="maven-publish")
+    apply(plugin=rootProject.libs.plugins.quilt.loom.get().pluginId)
+
+    dependencies {
+        minecraft(rootProject.libs.minecraft)
+        mappings(loom.layered {
+            addLayer(quiltMappings.mappings("org.quiltmc:quilt-mappings:${rootProject.libs.versions.quilt.mappings.get()}:v2"))
+        })
+
+        modImplementation(rootProject.libs.quilt.loader)
+
+        modImplementation(rootProject.libs.qsl)
+    }
+
+    tasks.remapJar {
+        archiveBaseName.set("quilt-kotlin-libraries-${name}")
+    }
+
+    publishing {
+        publications {
+            if (project.name != "wrapper") {
+                create<MavenPublication>("Maven") {
+                    groupId = "org.quiltmc.quilt-kotlin-libraries"
+                    artifactId = "quilt-kotlin-libraries-${project.name}"
+                    this.version = projectVersion
+                    from(components["java"])
+                }
+            }
+        }
+        repositories {
+            mavenLocal()
+            if (System.getenv("MAVEN_URL") != null) {
+                maven {
+                    setUrl(System.getenv("MAVEN_URL"))
+                    credentials {
+                        username = System.getenv("MAVEN_USERNAME")
+                        password = System.getenv("MAVEN_PASSWORD")
+                    }
+                    name = "Maven"
+                }
+            } else if (System.getenv("SNAPSHOTS_URL") != null) {
+                maven {
+                    setUrl(System.getenv("SNAPSHOTS_URL"))
+                    credentials {
+                        username = System.getenv("SNAPSHOTS_USERNAME")
+                        password = System.getenv("SNAPSHOTS_PASSWORD")
+                    }
+                    name = "Maven"
+                }
+            }
         }
     }
 }
