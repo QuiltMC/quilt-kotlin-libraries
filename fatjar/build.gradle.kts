b2db5905b66a14dedd9729d991afea181c5d000a
oliver <kookooloopdaking@gmail.com>
2022-06-11 13:07:18 -0500

    implement and include with fatjar to get transitive dependancy

--- fatjar/build.gradle.kts
+++ fatjar/build.gradle.kts
@@ -2,10 +2,14 @@ plugins {
     id("org.quiltmc.loom")
 }
 
+fun DependencyHandlerScope.includeApi(dependency: Any) {
+    include(dependency)?.let { implementation(it) }
+}
+
 dependencies {
-    include(project(":core"))
-    include(project(":wrapper:qsl"))
-    include(project(":wrapper:minecraft"))
+    includeApi(project(":core"))
+    includeApi(project(":wrapper:qsl"))
+    includeApi(project(":wrapper:minecraft"))
 }
 
 tasks.remapJar {
