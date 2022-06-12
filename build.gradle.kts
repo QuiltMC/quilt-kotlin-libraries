d277cdabf284d87feac3d14bc27f1512fe562947
NoComment <67918617+NoComment1105@users.noreply.github.com>
2022-06-12 17:42:55 +0100

    Fix typo

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ group = "org.quiltmc"
 version = project.version
 val projectVersion = project.version as String + if (System.getenv("SNAPSHOTS_URL") != null && System.getenv("MAVEN_URL") == null) "-SNAPSHOT" else ""
 
-val javaVersion = 17 // The current version of Java the Minecraft uses
+val javaVersion = 17 // The current version of Java used by Minecraft
 
 repositories {
     mavenCentral()
