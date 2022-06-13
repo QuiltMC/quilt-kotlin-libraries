b1126187e0d0ab9a5fd78a0d1f07b4ea22719a2a
sschr15 <steven.sschr15@hotmail.com>
2022-06-12 21:00:23 -0500

    the easiest part for commands

---
+++ wrapper/qsl/src/main/kotlin/org/quiltmc/qkl/wrapper/qsl/commands/CommandRegistration.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("unused")
+
+package org.quiltmc.qkl.wrapper.qsl.commands
+
+import com.mojang.brigadier.CommandDispatcher
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+import net.minecraft.command.CommandBuildContext
+import net.minecraft.server.command.CommandManager
+import net.minecraft.server.command.ServerCommandSource
+import org.quiltmc.qkl.wrapper.qsl.EventRegistration
+import org.quiltmc.qsl.command.api.CommandRegistrationCallback
+import org.quiltmc.qsl.command.api.client.ClientCommandRegistrationCallback
+import org.quiltmc.qsl.command.api.client.QuiltClientCommandSource
+
+public typealias CommandRegisterCallback<S> = CommandDispatcher<S>.(
+    buildContext: CommandBuildContext,
+    environment: CommandManager.RegistrationEnvironment
+) -> Unit
+
+public fun EventRegistration.onCommandRegistration(
+    callback: CommandRegisterCallback<ServerCommandSource>
+) {
+    CommandRegistrationCallback.EVENT.register(CommandRegistrationCallback(callback))
+}
+
+@Environment(EnvType.CLIENT)
+public fun EventRegistration.onClientCommandRegistration(
+    callback: CommandRegisterCallback<QuiltClientCommandSource>
+) {
+    ClientCommandRegistrationCallback.EVENT.register(ClientCommandRegistrationCallback(callback))
+}
