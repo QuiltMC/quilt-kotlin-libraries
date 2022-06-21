24bcf6d33fee4354f828aeb615b5948e21af6710
sschr15 <steven.sschr15@hotmail.com>
2022-06-21 12:51:24 -0500

    QSL sugar (#9)
    * QSL screen sugar
    
    * I just remembered typealiases can have type arguments
    
    * Crash report event sugar
    
    * Lifecycle event sugar
    
    * Mark screen sugar as client-only
    
    * Add tooltip sugar(?)
    
    * make a name more consistent with my other names
    
    * make detekt happy
    
    * clean up global namespace a bit
    also i moved the opt-in annotation haha
    
    * Clean up typealiases
    
    * Block extensions
    wow that's a lot of default parameters
    
    * Tags are simple
    
    * Licensing
    
    * Dimension API is smol right now
    
    * Resource loader events
    
    * so many docs
    
    * Item group and item setting sugar
    
    * Add builder functions for block extensions
    
    * Recipe events and extensions
    
    * whoops got the wrong package name
    
    * Implement requested changes
    
    * Change entity teleportation to be easier to understand
    
    * add null explanation
    
    * the easiest part for commands
    
    * the easiest™ part for registries
    
    * registry entry attachment?
    
    * make leo happy :speenapple:
    
    * Networking events
    the rest of networking is more pain™
    
    * Player lookup sugar is nice
    
    * Implement requested changes
    
    * sad i liked a contract better
    contracts are probably better for people who don't really understand `apply` somehow but emma@furrysys says otherwise
    
    * i attributed myself
    
    * implement more changes
    because it never ends
    
    * lambdaurora was looking at the wrong class
    it's okay we all make weird errors like this, especially when we need sleep
    
    * Remove @MustRunQuick
    
    * Change dimension stuff
    
    * add todo
    
    * change crash report typealiases
    
    * Add docs, default parameters, and a missing required param

---
+++ wrapper/qsl/src/main/kotlin/org/quiltmc/qkl/wrapper/qsl/resource/ResourceLoaderEvents.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2022 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("unused")
+
+package org.quiltmc.qkl.wrapper.qsl.resource
+
+import net.minecraft.resource.ResourceManager
+import net.minecraft.server.MinecraftServer
+import org.quiltmc.qkl.wrapper.qsl.EventRegistration
+import org.quiltmc.qsl.resource.loader.api.ResourceLoaderEvents
+
+public typealias DataPackReloadStart = (
+    /**
+     * The current server, or `null` if this is the first load.
+     */
+    server: MinecraftServer?,
+    /**
+     * The resource manager that is being reloaded, or `null` if this is the first load.
+     */
+    resourceManager: ResourceManager?,
+) -> Unit
+
+public typealias DataPackReloadFinish = (
+    /**
+     * The current server, or `null` if this is the first load.
+     */
+    server: MinecraftServer?,
+    /**
+     * The resource manager that is being reloaded, or `null` if this is the first load.
+     */
+    resourceManager: ResourceManager?,
+    /**
+     * An error that occurred during the reload, or `null` if no error occurred.
+     */
+    error: Throwable?,
+) -> Unit
+
+/**
+ * Called when data pack reloading is about to begin.
+ * @see ResourceLoaderEvents.START_DATA_PACK_RELOAD
+ * @see ResourceLoaderEvents.StartDataPackReload.onStartDataPackReload
+ *
+ * @author sschr15
+ */
+public fun EventRegistration.onDataPackReloadStart(callback: DataPackReloadStart) {
+    ResourceLoaderEvents.START_DATA_PACK_RELOAD.register(ResourceLoaderEvents.StartDataPackReload(callback))
+}
+
+/**
+ * Called when data pack reloading has finished.
+ * @see ResourceLoaderEvents.END_DATA_PACK_RELOAD
+ * @see ResourceLoaderEvents.EndDataPackReload.onEndDataPackReload
+ *
+ * @author sschr15
+ */
+public fun EventRegistration.onDataPackReloadFinish(callback: DataPackReloadFinish) {
+    ResourceLoaderEvents.END_DATA_PACK_RELOAD.register(ResourceLoaderEvents.EndDataPackReload(callback))
+}
