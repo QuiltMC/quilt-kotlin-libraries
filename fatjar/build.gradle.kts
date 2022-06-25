5e8807955b166e95438e9829b7ca9ea19af83a09
SilverAndro <52360088+SilverAndro@users.noreply.github.com>
2022-06-25 12:57:29 -0400

    Repurpose includeApi method

--- fatjar/build.gradle.kts
+++ fatjar/build.gradle.kts
@@ -2,14 +2,14 @@ plugins {
     id("org.quiltmc.loom")
 }
 
-fun DependencyHandlerScope.includeApi(dependency: Any) {
-    include(dependency)?.let { implementation(it) }
+fun DependencyHandlerScope.includeProject(projectId: String) {
+    include(modApi(project(projectId, configuration = "namedElements"))!!)
 }
 
 dependencies {
-    include(modApi(project(":core",              configuration = "namedElements"))!!)
-    include(modApi(project(":wrapper:qsl",       configuration = "namedElements"))!!)
-    include(modApi(project(":wrapper:minecraft", configuration = "namedElements"))!!)
+    includeProject(":core")
+    includeProject(":wrapper:qsl")
+    includeProject(":wrapper:minecraft")
 }
 
 tasks.remapJar {
