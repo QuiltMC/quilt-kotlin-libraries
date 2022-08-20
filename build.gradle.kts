508df2e463fc84a683facd75a0538e60eda0f0e3
oliver <kookooloopdaking@gmail.com>
2022-08-19 19:21:26 -0500

    Bump Kotlin, FLK, and bundled library versions, finalize versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ buildscript {
 }
 
 group = "org.quiltmc"
-version = project.version
+version = project.version.toString() + "+kt." + project.libs.versions.kotlin + "+flk.1.8.2"
 val projectVersion = project.version as String + if (System.getenv("SNAPSHOTS_URL") != null && System.getenv("MAVEN_URL") == null) "-SNAPSHOT" else ""
 
 val javaVersion = 17 // The current version of Java used by Minecraft
