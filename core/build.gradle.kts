26c6dd2021c6fec6e00b9c163f37ab0a8e7bf06a
SilverAndro <52360088+SilverAndro@users.noreply.github.com>
2023-04-11 09:57:30 -0400

    Move kotlinx libs to core to align with FLK

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,11 +1,22 @@
 fun DependencyHandlerScope.includeApi(dependency: Any) {
-    include(dependency)?.let { api(it) }
+    api(dependency)
+    include(dependency)
 }
 
 dependencies {
     val kotlinVersion = rootProject.libs.versions.kotlin.get()
+
     includeApi(kotlin("stdlib", kotlinVersion))
     includeApi(kotlin("stdlib-jdk7", kotlinVersion))
     includeApi(kotlin("stdlib-jdk8", kotlinVersion))
     includeApi(kotlin("reflect", kotlinVersion))
+
+    includeApi(rootProject.libs.coroutines.core.get())
+    includeApi(rootProject.libs.coroutines.jvm.get())
+    includeApi(rootProject.libs.coroutines.jdk8.get())
+    includeApi(rootProject.libs.serialization.core.get())
+    includeApi(rootProject.libs.serialization.json.get())
+    includeApi(rootProject.libs.serialization.cbor.get())
+    includeApi(rootProject.libs.atomic.get())
+    includeApi(rootProject.libs.datetime.get())
 }
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -3,18 +3,6 @@ plugins {
     alias(libs.plugins.serialization)
 }
 
-fun DependencyHandlerScope.includeApi(dependency: Any) {
-    include(dependency)?.let { api(it) }
-}
-
 dependencies {
     implementation(project(":core"))
-    includeApi(rootProject.libs.coroutines.core.get())
-    includeApi(rootProject.libs.coroutines.jvm.get())
-    includeApi(rootProject.libs.coroutines.jdk8.get())
-    includeApi(rootProject.libs.serialization.core.get())
-    includeApi(rootProject.libs.serialization.json.get())
-    includeApi(rootProject.libs.serialization.cbor.get())
-    includeApi(rootProject.libs.atomic.get())
-    includeApi(rootProject.libs.datetime.get())
 }
